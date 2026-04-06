d99018f91969cd49319283d722bf367bd227df83
hst1189 <hst1189@hotmail.com>
2026-04-07 02:12:21 +0900

    'update'

--- index.js
+++ index.js
@@ -3,37 +3,32 @@ const domains = ['grapehut.dpdns.org', 'gmeek.dpdns.org'];
 
 async function main() {
 
-  let browser;
-  let page;
+  const browser = await chromium.launch({
+    headless: false,
+    channel: 'chrome',
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-  try {
-    browser = await chromium.launch({
-      headless: true, // 是否启用无头模式，默认为true
-      viewport: { width: 1280, height: 720 }
-    });
-
-    page = await browser.newPage();
-    page.setDefaultTimeout(30000);
-
+  try {  	
     let i = Math.floor(Math.random() * domains.length);
     console.log(`📱 - 正在访问网站 ${domains[i]}...`);
-    await page.goto(`http://${domains[i]}`, { waitUntil: 'networkidle' });
-    await page.waitForTimeout(5000);
-
+    await page.setDefaultNavigationTimeout(60000); 
+    await page.goto(`https://${domains[i]}`);
+    page.locator('iframe[style*="z-index"]').nth(0).waitFor({ state: 'visible' }) 
+    
     console.log(`🔑 - 点击...`);
-    await page.mouse.click(640, 360)
-    await page.waitForTimeout(5000);
-
-    await page.waitForLoadState('networkidle');
-    await page.waitForTimeout(5000);
+    await page.locator('iframe[style*="z-index"]').nth(0).click();
+    await page.waitForTimeout(10000);
 
   } catch (e) {
     console.log(`❌ - 异常: ${e.message}`);
 
   } finally {
-    if (page) await page.close();
-    await browser.close();
     console.log(`🎉 - 网页关闭`);
+    if (page) await page.close();
+    if (context) await context.close();
+    if (browser) await browser.close();
   }
 }
 
